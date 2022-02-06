1f16e0dc702be8b98f09d7fb76065c35683f1d7f
Payal Bhandwalkar <bhandwalkar.payal@gmail.com>
2022-02-06 15:41:29 +0530

    created a moduxi react app and have modified the navbar

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+const Header = () => {
+  return <div className="gpt3__header section__padding" >
+    <div className="">
+   <h1>
+
+    </h1>
+    </div>
+    </div>;
+};
+
+export default Header;
